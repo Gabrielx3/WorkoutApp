21e0f576da505e75421b962d5a744ead0cacb9ea
Gabriele Guerinoni <gabriele.guerinoni@stu.istitutoturoldo.edu.it>
2026-05-23 12:27:22 +0200

    commit 1

---
+++ app/src/main/java/com/example/workoutapp/model/Exercise.java
@@ -0,0 +1,24 @@
+package com.example.workoutapp.model;
+import java.io.Serializable;
+
+public class Exercise implements Serializable {private String nome;
+    private int serie, ripetizioni, secondiRecupero;
+
+    public Exercise(String nome, int serie, int ripetizioni, int secondiRecupero) {
+        this.nome = nome;
+        this.serie = serie;
+        this.ripetizioni = ripetizioni;
+        this.secondiRecupero = secondiRecupero;
+    }
+
+    // Metodi per leggere i dati
+    public String getNome() { return nome; }
+    public int getSerie() { return serie; }
+    public int getRipetizioni() { return ripetizioni; }
+    public int getSecondiRecupero() { return secondiRecupero; }
+
+    @Override
+    public String toString() {
+        return nome + " (" + serie + "x" + ripetizioni + ")";
+    }
+}
