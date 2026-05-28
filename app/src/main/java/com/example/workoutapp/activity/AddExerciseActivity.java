4f41246c26196a96bf7c56f4b82a792a6e52218b
Gabrielx3 <scottixgabriel@gmail.com>
2026-05-28 09:04:53 +0200

    creazione cartella activity dove verranno inserite le varie activity dell app

--- app/src/main/java/com/example/workoutapp/activity/AddExerciseActivity.java
+++ app/src/main/java/com/example/workoutapp/activity/AddExerciseActivity.java
@@ -1,4 +1,4 @@
-package com.example.workoutapp;
+package com.example.workoutapp.activity;
 
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +7,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.workoutapp.R;
 
 public class AddExerciseActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/workoutapp/activity/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.workoutapp;
+package com.example.workoutapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.workoutapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
